f27b92e286f0a8131959a1a7e1128819e1df6e02
shubhampachghare <shubhampachghare@gmail.com>
2022-11-20 20:15:23 +0530

    [Shubham]- Adding Arithmatic_Compute2

--- UC1_user_inputs.sh
+++ UC1_user_inputs.sh
@@ -4,4 +4,13 @@ read -p "Enter 1st Number" a
 read -p "Enter 2nd Number" b
 read -p "Enter 3rd Number" c
 
-echo "$(($a+$b*$c))"
+
+echo "Arithematic compute one is: $(($a+$b*$c))"
+
+
+function ArithmaticCompute2()
+{
+echo "Arithmatic compute is :$(($a*$b+$c))"
+} 
+
+ArithmaticCompute2
